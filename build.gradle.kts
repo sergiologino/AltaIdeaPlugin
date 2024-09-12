88b3d759074f06a3c9021e3eb109d2dca7b48c84
savki <savkin@altacod.ru>
2024-09-12 23:21:46 +0300

    build.gradle.kts added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,42 +1,28 @@
 plugins {
-    id("java")
-    id("org.jetbrains.intellij") version "1.5.2"
+    id("org.jetbrains.intellij") version "1.2.0" // Плагин для разработки плагинов IntelliJ IDEA
+    java
 }
 
-group = "com.example"
+group = "com.example.chatplugin"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+    mavenCentral() // Централизованный репозиторий Maven
 }
 
-// Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-intellij {
-    version.set("2021.2")
-    type.set("IC") // Target IDE Platform
+dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.9.0") // Для работы с API
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0") // Для обработки JSON
+}
 
-    plugins.set(listOf(/* Plugin Dependencies */))
+intellij {
+    version.set("20244.1.3") // Версия IntelliJ IDEA
+    type.set("IC") // Community Edition или Ultimate Edition
 }
 
 tasks {
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
-    }
-
     patchPluginXml {
-        sinceBuild.set("212")
-        untilBuild.set("222.*")
-    }
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
+        sinceBuild.set("221")
+        untilBuild.set("223.*")
     }
 }
